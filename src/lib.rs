1a31763642cd39e4bdc74ec3821d5f3eb231986a
Kaa <>
2023-03-02 14:46:01 +0100

    Added ShellCommand struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+use std::process::{Command, Stdio};
+
 pub fn about() -> String {
     let str1 = "NoShell is a simple wrapper for std::process::Command;";
     let str2 = "For using rust as a replacement for shell scripting language.";
     format!("{}\n{}", str1, str2)
+}
+
+pub struct ShellCommand {
+    cmd: Command,
+    stdin: Option<Stdio>,
+    args: Vec<String>,
 }
