7097c95eb4b7c456883e88236b448ddb86d7eb0c
Kaa <>
2023-03-02 21:57:18 +0100

    Added tests that test running of commands

--- src/tests.rs
+++ src/tests.rs
@@ -7,7 +7,6 @@ fn create_cmd() {
     let b = ShellCommand {
         stdin: None,
         argv: vec!["echo".into()],
-        output: None,
     };
 
     assert_eq!(a, b)
@@ -23,4 +22,66 @@ fn add_args() {
     assert_eq!(a, b);
     assert_eq!(c, d);
     assert_eq!(b, d);
+}
+
+#[test]
+fn string_pipe() {
+    let sed = ShellCommand::new("sed")
+        .args(["-e", "s|dfg|qwerty|g"])
+        .pipe_string("asdfghjkl");
+    let manual_sed = ShellCommand {
+        stdin: Some("asdfghjkl".into()),
+        argv: vec!["sed".into(), "-e".into(), "s|dfg|qwerty|g".into()],
+    };
+
+    assert_eq!(sed, manual_sed);
+    assert_eq!(sed.run().unwrap(), manual_sed.run().unwrap());
+}
+
+#[test]
+fn simple_run() {
+    let sed = ShellCommand::new("sed")
+        .args(["-e", "s|dfg|qwerty|g"])
+        .pipe_string("asdfghjkl")
+        .run()
+        .unwrap();
+    assert_eq!("asqwertyhjkl", sed);
+}
+
+#[test]
+fn multiple_run() {
+    let sed = ShellCommand::new("sed")
+        .args(["-e", "s|dfg|qwerty|g"])
+        .pipe_string("asdfghjkl");
+
+    let ans1 = sed.run().unwrap();
+    let ans2 = sed.run().unwrap();
+
+    assert_eq!(ans1, ans2);
+}
+
+#[test]
+fn get_stderr() {
+    let sed_err = match ShellCommand::new("sed").run() {
+        Ok(_) => "",
+        Err(_) => "Error detected",
+    };
+
+    assert_eq!(sed_err, "Error detected");
+}
+
+#[test]
+fn cmd_pipe() {
+    let sed = ShellCommand::new("sed")
+        .args(["-e", "s|dfg|qwerty|g"])
+        .pipe_string("asdfghjkl");
+
+    let sed2 = ShellCommand::new("sed")
+        .args(["-e", "s|as|qwerty|g"])
+        .pipe_stdout(sed)
+        .unwrap()
+        .run()
+        .unwrap();
+
+    assert_eq!("qwertyqwertyhjkl", sed2);
 }
